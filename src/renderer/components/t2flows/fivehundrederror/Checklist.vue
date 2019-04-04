866dcee4c76d5be83f111c98f92cad5a1110f5c9
Steven Pastrana <steven1096@godaddy.com>
2019-04-04 14:34:50 -0700

    fix router

--- src/renderer/components/t2flows/fivehundrederror/Checklist.vue
+++ src/renderer/components/t2flows/fivehundrederror/Checklist.vue
@@ -1,23 +1,53 @@
 <template>
   <div>
   <ul>
-    <li>Check for Malware</li>
-    <li>Disable Control Files</li>
-    <li>Enable Debug Mode</li>
-    <li>Disable Plugin/Themes</li>
-    <li>Clear Cache / Kill Processes / Toggle PHP</li>
-    <li>Resubmit / Sandbox and Resubmit</li>
+    <li>
+      <input type="checkbox" id="checkbox" v-model="checked_01">
+      <label v-bind:class="{strike: checked_01}" for="checkbox">Check for Malware</label>
+    </li>
+    <li>
+      <input type="checkbox" id="checkbox" v-model="checked_02">
+      <label v-bind:class="{strike: checked_02}" for="checkbox">Disable Control Files</label>
+    </li>
+    <li>
+      <input type="checkbox" id="checkbox" v-model="checked_03">
+      <label v-bind:class="{strike: checked_03}" for="checkbox">Enable Debug Mode</label>
+    </li>
+    <li>
+      <input type="checkbox" id="checkbox" v-model="checked_04">
+      <label v-bind:class="{strike: checked_04}" for="checkbox">Disable Plugin/Themes</label>
+    </li>
+    <li>
+      Clear Cache / Kill Processes / Toggle PHP
+    </li>
+    <li>
+      Resubmit / Sandbox and Resubmit
+    </li>
   </ul>
 </div>
 </template>
 
 <script>
 export default{
-  // name: checklist
+  data(){
+    return{
+      checked_01: false,
+      checked_02: false,
+      checked_03: false,
+      checked_04: false,
+      checked_05: false,
+      checked_06: false,
+    }
+  }
 }
 
 </script>
 
-<style>
-
+<style scoped>
+.strike{
+  text-decoration: line-through;
+}
+.no-style{
+  list-style-type: none;
+}
 </style>
